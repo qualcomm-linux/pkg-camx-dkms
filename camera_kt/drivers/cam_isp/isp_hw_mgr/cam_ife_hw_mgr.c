18b85b4588111aa78d1573cc041e523b279e2de5
Keval Sanghvi <ksanghvi@qti.qualcomm.com>
2026-02-18 11:24:10 +0530

    msm: camera: isp: Fix KMD buffer handle in IFE prepare
    The ISP context cleanup logic in __cam_isp_ctx_move_req_to_free_list
    relies on hw_update_data.kmd_cmd_buff_info to retrieve the buffer
    handle and decrement the reference count.
    
    Currently, in cam_ife_mgr_prepare_hw_update, the KMD buffer
    information is retrieved into a local stack variable kmd_buf
    As a result, the persistent prepare_hw_data structure is never
    populated with the correct buffer handle.
    
    This causes cam_mem_put_kref to be called with an invalid handle
    during request cleanup, leading to memory reference leaks, Color
    corruption, flickering, and camera stream freeze after several
    iterations due to memory exhaustion
    
    Fix this by removing the local variable and populating
    prepare_hw_data->kmd_cmd_buff_info directly during the hardware
    prepare phase.
    
    CRs-Fixed: 4399397, 4424334
    Change-Id: Ic6362c4681bd25e0e2e22017e5e042d2570bd028
    Signed-off-by: Keval Sanghvi <ksanghvi@qti.qualcomm.com>

--- camera_kt/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ camera_kt/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -6711,7 +6711,6 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 
 	struct cam_ife_hw_mgr_ctx               *ctx;
 	struct cam_ife_hw_mgr                   *hw_mgr;
-	struct cam_kmd_buf_info                  kmd_buf;
 	uint32_t                                 i;
 	bool                                     fill_fence = true;
 	bool                                     frame_header_enable = false;
@@ -6740,12 +6739,12 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 		return rc;
 
 	/* Pre parse the packet*/
-	rc = cam_packet_util_get_kmd_buffer(prepare->packet, &kmd_buf);
+	rc = cam_packet_util_get_kmd_buffer(prepare->packet, &prepare_hw_data->kmd_cmd_buff_info);
 	if (rc)
 		return rc;
 
 	if (ctx->custom_config & CAM_IFE_CUSTOM_CFG_FRAME_HEADER_TS) {
-		rc = cam_ife_mgr_util_insert_frame_header(&kmd_buf,
+		rc = cam_ife_mgr_util_insert_frame_header(&prepare_hw_data->kmd_cmd_buff_info,
 			prepare_hw_data);
 		if (rc)
 			return rc;
@@ -6793,7 +6792,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 			change_base_info.cdm_id = ctx->cdm_id;
 			rc = cam_isp_add_change_base(prepare,
 				&ctx->res_list_ife_src,
-				&change_base_info, &kmd_buf);
+				&change_base_info, &prepare_hw_data->kmd_cmd_buff_info);
 			if (rc) {
 				CAM_ERR(CAM_ISP,
 				"Failed in change base i=%d, idx=%d, rc=%d",
@@ -6803,7 +6802,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 		}
 		/* get command buffers */
 		if (ctx->base[i].split_id != CAM_ISP_HW_SPLIT_MAX) {
-			rc = cam_isp_add_command_buffers(prepare, &kmd_buf,
+			rc = cam_isp_add_command_buffers(prepare, &prepare_hw_data->kmd_cmd_buff_info,
 				&ctx->base[i],
 				cam_isp_packet_generic_blob_handler,
 				ctx->res_list_ife_out, max_ife_out_res);
@@ -6829,7 +6828,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 			hw_mgr->mgr_common.img_iommu_hdl,
 			hw_mgr->mgr_common.img_iommu_hdl_secure,
 			prepare, ctx->base[i].idx,
-			&kmd_buf, ctx->res_list_ife_out,
+			&prepare_hw_data->kmd_cmd_buff_info, ctx->res_list_ife_out,
 			&ctx->res_list_ife_in_rd,
 			max_ife_out_res, fill_fence,
 			&frame_header_info);
@@ -6919,7 +6918,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 		if (!ctx->internal_cdm) {
 			rc = cam_isp_add_change_base(prepare,
 				&ctx->res_list_ife_src,
-				&change_base_info, &kmd_buf);
+				&change_base_info, &prepare_hw_data->kmd_cmd_buff_info);
 
 			if (rc) {
 				CAM_ERR(CAM_ISP,
@@ -6930,7 +6929,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 		}
 		/*Add reg update */
 		rc = cam_isp_add_reg_update(prepare, &ctx->res_list_ife_src,
-			ctx->base[i].idx, &kmd_buf);
+			ctx->base[i].idx, &prepare_hw_data->kmd_cmd_buff_info);
 
 		if (rc) {
 			CAM_ERR(CAM_ISP,
@@ -6944,7 +6943,7 @@ static int cam_ife_mgr_prepare_hw_update(void *hw_mgr_priv,
 	if (prepare->num_out_map_entries && prepare->num_in_map_entries &&
 		ctx->is_offline) {
 		rc = cam_isp_add_go_cmd(prepare, &ctx->res_list_ife_in_rd,
-			ctx->base[i].idx, &kmd_buf);
+			ctx->base[i].idx, &prepare_hw_data->kmd_cmd_buff_info);
 		if (rc)
 			CAM_ERR(CAM_ISP,
 				"Add GO_CMD faled i: %d, idx: %d, rc: %d",
